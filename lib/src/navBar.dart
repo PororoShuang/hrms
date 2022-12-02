a5afac5bdd6cf5c46fcdfc43aa818a849ba289b1
Andrea Shuang <limzs-wm19@student.tarc.edu.my>
2022-12-02 17:12:15 +0800

    categorised

--- lib/src/navBar.dart
+++ lib/src/navBar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'AccountManagement/View/home.dart';
-import 'attendance.dart';
+import '../src/Attendance/View/attendance.dart';
 
 class BottomNavigation extends StatefulWidget {
   const BottomNavigation({super.key});
@@ -12,45 +12,39 @@ class BottomNavigation extends StatefulWidget {
 class _BottomNavigation extends State<BottomNavigation> {
   int _selectedIndex = 0;
 
-  final screens=[
+  final screens = [
     Home(),
     Attendance(),
     Home(),
   ];
   @override
-  Widget build(BuildContext context)=> Scaffold (
-    body: screens[_selectedIndex],
-      bottomNavigationBar: NavigationBarTheme(
-        data: NavigationBarThemeData(
-          labelTextStyle: MaterialStateProperty.all(
-            TextStyle(fontSize: 14,fontWeight: FontWeight.w500),
-          )
+  Widget build(BuildContext context) => Scaffold(
+        body: screens[_selectedIndex],
+        bottomNavigationBar: NavigationBarTheme(
+          data: NavigationBarThemeData(
+              labelTextStyle: MaterialStateProperty.all(
+            TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+          )),
+          child: NavigationBar(
+            height: 60,
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: (_selectedIndex) =>
+                setState(() => this._selectedIndex = _selectedIndex),
+            destinations: [
+              NavigationDestination(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.library_add_check_outlined),
+                label: 'Attendance',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.account_circle),
+                label: 'Account',
+              ),
+            ],
+          ),
         ),
-        child:NavigationBar(
-          height:60,
-          selectedIndex: _selectedIndex,
-          onDestinationSelected: (_selectedIndex)=>
-          setState(() => this._selectedIndex=_selectedIndex),
-          destinations: [
-            NavigationDestination(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.library_add_check_outlined),
-              label: 'Attendance',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.account_circle),
-              label: 'Account',
-            ),
-          ],
-        ),
-      ),
-  );
+      );
 }
-
-
-
-
-
