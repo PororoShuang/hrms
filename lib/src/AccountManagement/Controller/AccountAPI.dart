d67f4228a45f2ba40133a14a21b72d5c212e9558
yl07 <liewyl-wm19@student.tarc.edu.my>
2022-12-18 19:35:41 +0800

    testing acc

--- lib/src/AccountManagement/Controller/AccountAPI.dart
+++ lib/src/AccountManagement/Controller/AccountAPI.dart
@@ -5,11 +5,13 @@ import 'package:hrms/src/AccountManagement/Model/employee.dart';
 import 'package:hrms/src/Attendance/constants.dart';
 import 'package:hrms/src/Attendance/Model/attendance_information.dart';
 import 'package:http/http.dart' as http;
+import 'package:hrms/src/Authentication/View/login_screen.dart';
 
 class ApiService {
   Future<List<Employee>?> getUsers() async {
     try {
       //var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+
       var url = Uri.parse(
           "https://finalyearproject20221212223004.azurewebsites.net/api/EmployeeAPI");
 
@@ -30,52 +32,56 @@ class ApiService {
           List<String> test = element.split(",");
           //int index = 0;
           int i = -1;
-          model.employeeId = "E00002";
+          model.employeeId = currentLoginID;
           String captureId = test[++i]; //
           if (model.employeeId == captureId) {
+            model.setEmployeeId = captureId;
+
             //model.employeeId = test[++i];
-            model.employeeIdByCompany = test[++i]; //
+            model.setEmployeeIdByCompany = test[++i]; //
             //model.employeeName = test[++i];
-            model.userId = test[++i]; //
-            model.parentCompany = test[++i]; //
+            model.setUserId = test[++i]; //
+            model.setParentCompany = test[++i]; //
             //model.company = test[++i];
-            model.staffRole = test[++i]; //
+            model.setStaffRole = test[++i]; //
             //model.role = test[++i];
-            model.accPass = test[++i]; //
-            model.employerId = test[++i]; //
+            model.setAccPass = test[++i]; //
+            model.setEmployerId = test[++i]; //
             //model.employer = test[++i];
-            model.employmentStartDate = test[++i]; //
+            model.setEmploymentStartDate = test[++i]; //
             //model.employmentStartDate = DateTime.parse(test[++i]);
-            model.typesOfWages = test[++i];
-            model.wagesRate = test[++i];
+            model.setTypesOfWages = test[++i];
+            model.setWagesRate = test[++i];
             //model.wagesRate = double.parse(test[++i]);
-            model.employementLetter = test[++i];
+            model.setEmployementLetter = test[++i];
             // String bool1 = test[++i];
             // if (bool1.toLowerCase() == false)
             //   model.employementLetter = false;
             // else
             //   model.employementLetter = true;
-            model.monthlyDeduction = test[++i];
+            model.setMonthlyDeduction = test[++i];
             //model.isActive = test[++i];
-            model.icNo = test[i++];
-            model.dob = test[++i];
-            model.gender = test[i++];
-            model.nationality = test[i++];
-            model.phoneNo = test[i++];
-            model.email = test[i++];
-            model.epfNo = test[i++];
-            model.soscoNo = test[i++];
-            model.itaxNo = test[i++];
-            model.bankName = test[++i];
-            model.bankNo = test[++i];
-
-            model.religion = test[++i]; //
-            model.paidLeaveHourLeft = test[++i]; //
-            model.paidLeaveOnBargain = test[++i]; //
-            model.sickLeaveHourLeft = test[++i]; //
-            model.sickLeaveOnBargain = test[++i]; //
-            model.uuid = test[++i]; //
-            model.leaveUpdate = test[++i]; //
+            model.setIcNo = test[i++];
+            model.setDob = test[++i];
+            model.setGender = test[i++];
+            model.setNationality = test[i++];
+            model.setPhoneNo = test[i++];
+            model.setEmail = test[i++];
+            model.setEpfNo = test[i++];
+            model.setSoscoNo = test[i++];
+            model.setItaxNo = test[i++];
+            model.setBankName = test[++i];
+            model.setBankNo = test[++i];
+            model.setAspId = test[++i];
+            model.setProfileImgPath = test[++i];
+            model.setIsActive = test[++i];
+            model.setReligion = test[++i]; //
+            model.setPaidLeaveHourLeft = test[++i]; //
+            model.setPaidLeaveOnBargain = test[++i]; //
+            model.setSickLeaveHourLeft = test[++i]; //
+            model.setSickLeaveOnBargain = test[++i]; //
+            model.setUuid = test[++i]; //
+            model.setLeaveUpdate = test[++i]; //
 
             employee.add(model);
             break;
@@ -84,6 +90,7 @@ class ApiService {
           }
         }
         //);
+
         return employee;
         // List<Employee> storeInfo = [];
         // for (int i = 0; i < infoString.length; i++) {
