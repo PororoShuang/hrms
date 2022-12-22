5dac139d3c6dc141a5306db4979041ec0885f964
yl07 <liewyl-wm19@student.tarc.edu.my>
2022-12-23 02:14:29 +0800

    display shift details

--- lib/src/ShiftManagement/View/shift.dart
+++ lib/src/ShiftManagement/View/shift.dart
@@ -1,41 +1,109 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hrms/src/Attendance/Controller/AttendanceAPI.dart';
+import '../../Attendance/Model/attendance_information.dart';
 
-
-class Shift extends StatefulWidget{
+class Shift extends StatefulWidget {
   const Shift({super.key});
   @override
   State<Shift> createState() => _Shift();
 }
 
 class _Shift extends State<Shift> {
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  List<String> shiftTime = [];
+  List<Attendance> itemsShift = [];
+
+  void getData() async {
+    itemsShift = (await AttendanceApiService().getAttendance())!;
+
+    // for (int i = 0; i < itemsShift.length; i++) {
+    //   shiftTime.add(itemsShift[i].shift_date_.toString() +
+    //       "\n " +
+    //       itemsShift[i].supposed_start_.toString() +
+    //       " - " +
+    //       itemsShift[i].supposed_end_.toString() +
+    //       "&" +
+    //       itemsShift[i].attendance_id_.toString());
+    // }
+    Future.delayed(const Duration(seconds: 5)).then((value) => setState(() {}));
+  }
 
   @override
-  Widget build(BuildContext context)=> Scaffold(
-    appBar: AppBar(
-      title: Text('Shift'),
-      backgroundColor: Colors.blueGrey[900],
-      centerTitle:true,
-      automaticallyImplyLeading: true,
-      leading: IconButton(icon:Icon(Icons.arrow_back),
-        onPressed: () {
-          Navigator.pop(context); },),
-    ),
-    body: Center(
-      child: Column(
-          children:<Widget>[
-            SizedBox(height: 30),
-            Text("Monday",style: TextStyle(height:2,fontSize:20,color: Colors.black45)),
-            Card(child:ListTile(
-                title: Text("Date"),
-                subtitle: Text("Time"))),
-            Card(child:ListTile(
-                title: Text("Date"),
-                subtitle: Text("Time"))),
-          ]
-      ),
-    ),
-
-  );
-
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Shift'),
+          backgroundColor: Colors.blueGrey[900],
+          centerTitle: true,
+          automaticallyImplyLeading: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: ListView(
+          children: itemsShift.map((e) {
+            return Card(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: 40.0),
+                        child: Text("Shift Date:"),
+                      ),
+                      Container(
+                        child: Text(e.shiftdate_),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: 40.0),
+                        child: Text("Shift Time:"),
+                      ),
+                      Container(
+                        child: (Text(" " +
+                            e.supposed_start_! +
+                            " - " +
+                            e.supposed_end_!)),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }).toList(),
+          // children: itemsShift.map((e) {
+          //   return Card(
+          //     child: Column(children: <Widget>[
+          //       SizedBox(height: 30),
+          //       // Text("Monday",
+          //       //     style: TextStyle(
+          //       //         height: 2, fontSize: 20, color: Colors.black45)),
+          //       Card(
+          //           child:
+          //               ListTile(title: Text(itemsShift), subtitle: Text("Time"))),
+          //       Card(
+          //           child:
+          //               ListTile(title: Text("Date"), subtitle: Text("Time"))),
+          //     ]),
+          //   );
+          // }).toList(),
+        ),
+      );
 }
